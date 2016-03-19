baca9dbd93dc612fe8f83905412b80da0d239aa1
Songbird0 <ci.hir@laposte.net>
2016-03-19 03:16:28 +0100

    0.1.15_4-ALPHA
    Complétion mineure du générateur de fichiers yaml. (pas de tests
    effectués)

--- src/fr/songbird/groovyresources/YamlFileSkeleton.groovy
+++ src/fr/songbird/groovyresources/YamlFileSkeleton.groovy
@@ -1,6 +1,14 @@
-package fr.songbird.groovyresources
+package fr.songbird.groovyresources;
 
-import org.yaml.snakeyaml.Yaml
+import org.yaml.snakeyaml.Yaml;
+
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.FileWriter;
+
+
+import static fr.songbird.manager.ConquestPlugin.LOGGER;
 
 class YamlFileSkeleton 
 {
@@ -23,7 +31,62 @@ class YamlFileSkeleton
 		]
 		
 		def yaml = new Yaml();
+
+		def yString = yaml.dump(yamlSkeleton);
+
+		FileWriter writer = null;
+		try
+		{
+			writer = new FileWriter(yamlFilePath);
+			writer.write(yString);
+		}
+		catch(FileNotFoundException fnf0)
+		{
+			LOGGER.error(fnf0.getMessage());
+		}
+		catch(IOException ioe0)
+		{
+			LOGGER.error(ioe0.getMessage());
+		}
+		finally
+		{
+			writer.flush();
+			writer.close();
+		}
+
 		
+	}
+
+	/**
+	* Generation d'un fichier yaml basé sur la map passée en paramètre par l'utilisateur: genericYamlFileArchetype(def maMap = [:])
+	*/
+	public void genericYamlFileArchetype(genericYamlSkeleton)
+	{
+		if(genericYamlSkeleton in java.util.Map)
+		{
+			FileWriter writer = null;
+			def yaml = new Yaml();
+			def yString = yaml.dump(genericYamlSkeleton);
+			try 
+			{
+				writer = new FileWriter(yamlFilePath);
+				writer.write(yString);
+			}
+			catch(FileNotFoundException fnf1) 
+			{
+				LOGGER.error(fnf1.getMessage());	
+			}
+			catch(IOException ioe1)
+			{
+				LOGGER.error(ioe1.getMessage());
+			}
+			finally
+			{
+				writer.flush();
+				writer.close();
+			}
+			
+		}
 	}
 	
 }
