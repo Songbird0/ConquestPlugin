278995ab3a8bc16cce228f799ebafb015d3bfea1
Songbird0 <ci.hir@laposte.net>
2016-04-03 18:08:02 +0200

    0.3.41_9-ALPHA: Ajout d'une interface encapsulant les mots-clés supportés par le générateur.

---
+++ src/fr/songbird/groovyresources/YamlFileSkeletonKeywords.java
@@ -0,0 +1,7 @@
+package fr.songbird.groovyresources;
+
+public interface YamlFileSkeletonKeywords 
+{
+	public final String sqlKeyword = new String("mysql.skeleton");
+	public final String genericKeyword = new String("generic.skeleton");
+}
