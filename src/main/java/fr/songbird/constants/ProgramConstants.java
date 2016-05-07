2d31ce8850e5e8d0fdf24e7e1aa21c0de974db93
Songbird0 <ci.hir@laposte.net>
2016-05-07 19:11:56 +0200

    0.6.57_11-alpha

--- src/main/java/fr/songbird/constants/ProgramConstants.java
+++ src/main/java/fr/songbird/constants/ProgramConstants.java
@@ -10,9 +10,9 @@ import java.io.File;
 public interface ProgramConstants
 {
 
-	public final String COMMANDARG = new String ("join");
+	public final String COMMANDARG = "join";
 	public final String[] NATIONNAME = new String[]{"Lutha", "Gondar"};
-	public final String COMMANDCORE = new String("nation");
+	public final String COMMANDCORE = "nation";
 	public final File CONFIGFILEPATH = new File(new StringBuffer().append(System.getProperty("user.home")).append(File.separator).append("ConfigConquestPlugin").toString());
 
 }
