64ed418f12e8d1c2aa62841fddc2e0f1d2c46b72
Songbird0 <ci.hir@laposte.net>
2016-05-07 20:07:53 +0200

    0.6.68_13-alpha
    Ajout des chemins des fichiers de configurations en tant que constantes dans l'interface ProgramConstants.

--- src/main/java/fr/songbird/constants/ProgramConstants.java
+++ src/main/java/fr/songbird/constants/ProgramConstants.java
@@ -14,5 +14,7 @@ public interface ProgramConstants
 	public final String[] NATIONNAME = new String[]{"Lutha", "Gondar"};
 	public final String COMMANDCORE = "nation";
 	public final File CONFIGFILEPATH = new File(new StringBuffer().append(System.getProperty("user.home")).append(File.separator).append("ConfigConquestPlugin").toString());
+	final File genericConfigFile = new File(new StringBuffer().append(CONFIGFILEPATH.toString()).append(File.separator).append("config.yml").toString());
+    final File mysqlConfigFile = new File(new StringBuffer().append(CONFIGFILEPATH.toString()).append(File.separator).append("mysqlconfig.yml").toString());
 
 }
