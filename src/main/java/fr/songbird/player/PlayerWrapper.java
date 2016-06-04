93bf6c2d047f0db3d1e04ea91e833ef892eb03d0
Songbird0 <ci.hir@laposte.net>
2016-06-04 04:51:04 +0200

    0.15.131_15-alpha
    Ajout d'un accesseur nécessaires aux plugins rattachés au système de conquêtes.

--- src/main/java/fr/songbird/player/PlayerWrapper.java
+++ src/main/java/fr/songbird/player/PlayerWrapper.java
@@ -72,6 +72,14 @@ public class PlayerWrapper
     }
 
     /**
+     * Renvoie les points de bataille du joueur.
+     */
+    public int getBattlePoints()
+    {
+        return battlePoints;
+    }
+
+    /**
      * Ajoute des points d'honneur au joueur.
      * @param honorPoints Nombre de points d'honneur à ajouter.
      */
