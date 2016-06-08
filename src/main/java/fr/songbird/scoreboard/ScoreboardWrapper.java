fd23a644118e6707d7ae5595f8fc83a6bfb40e7d
Songbird0 <ci.hir@laposte.net>
2016-06-08 06:54:07 +0200

    0.19.143_16-alpha
    Méthode getAdequateColor() réutilisée.

--- src/main/java/fr/songbird/scoreboard/ScoreboardWrapper.java
+++ src/main/java/fr/songbird/scoreboard/ScoreboardWrapper.java
@@ -1,34 +1,124 @@
-package main.java.fr.songbird.scoreboard;
+    private ChatColor getAdequateColor(String word)
+    {
+        String wordUC = word.toUpperCase();
+        if(wordUC.contains("AQUA"))
+        {
+            colorPrefix = "AQUA";
+            return ChatColor.AQUA;
+        }
+        else if(wordUC.contains("BLACK"))
+        {
+            colorPrefix = "BLACK";
+            return ChatColor.BLACK;
+        }
+        else if(wordUC.contains("BLUE"))
+        {
+            colorPrefix = "BLUE";
+            return ChatColor.BLUE;
+        }
+        else if(wordUC.contains("BOLD"))
+        {
+            colorPrefix = "BOLD";
+            return ChatColor.BOLD;
+        }
+        else if(wordUC.contains("DARK_AQUA"))
+        {
+            colorPrefix = "DARK_AQUA";
+            return ChatColor.DARK_AQUA;
+        }
+        else if(wordUC.contains("DARK_BLUE"))
+        {
+            colorPrefix = "DARK_BLUE";
+            return ChatColor.DARK_BLUE;
+        }
+        else if(wordUC.contains("DARK_GRAY"))
+        {
+            colorPrefix = "DARK_GRAY";
+            return ChatColor.DARK_GRAY;
+        }
+        else if(wordUC.contains("DARK_GREEN"))
+        {
+            colorPrefix = "DARK_GREEN";
+            return ChatColor.DARK_GREEN;
+        }
+        else if(wordUC.contains("DARK_PURPLE"))
+        {
+            colorPrefix = "DARK_PURPLE";
+            return ChatColor.DARK_PURPLE;
+        }
+        else if(wordUC.contains("DARK_RED"))
+        {
+            colorPrefix = "DARK_RED";
+            return ChatColor.DARK_RED;
+        }
+        else if(wordUC.contains("GOLD"))
+        {
+            colorPrefix = "GOLD";
+            return ChatColor.GOLD;
+        }
+        else if(wordUC.contains("GRAY"))
+        {
+            colorPrefix = "GRAY";
+            return ChatColor.GRAY;
 
-import main.java.fr.songbird.config.ConfigYamlFile;
-import main.java.fr.songbird.constants.ProgramConstants;
-import main.java.fr.songbird.exceptions.DataIntegrityException;
-import net.wytrem.logging.BasicLogger;
-import net.wytrem.logging.LoggerFactory;
-import org.bukkit.Bukkit;
-import org.bukkit.scoreboard.Scoreboard;
+        }
+        else if(wordUC.contains("GREEN"))
+        {
+            colorPrefix = "GREEN";
+            return ChatColor.GREEN;
+        }
+        else if(wordUC.contains("ITALIC"))
+        {
+            colorPrefix = "ITALIC";
+            return ChatColor.ITALIC;
+        }
+        else if(wordUC.contains("LIGHT_PURPLE"))
+        {
+            colorPrefix = "LIGHT_PURPLE";
+            return ChatColor.LIGHT_PURPLE;
+        }
+        else if(wordUC.contains("MAGIC"))
+        {
+            colorPrefix = "MAGIC";
+            return ChatColor.MAGIC;
+        }
+        else if(wordUC.contains("RED"))
+        {
+            colorPrefix = "RED";
+            return ChatColor.RED;
+        }
+        else if(wordUC.contains("YELLOW"))
+        {
+            colorPrefix = "YELLOW";
+            return ChatColor.YELLOW;
+        }
 
-/**
- * Created by songbird on 30/05/16.<br>
- * Encapsule les fonctionnalités du scoreboard pour y enregistrer divers objectifs et effectuer des opérations<br>
- * non couvertes par les outils de spigot.
- *
- * @author Songbird
- */
-public class ScoreboardWrapper implements ProgramConstants
-{
+        return null;
+    }
+
     /**
-     * Reférence vers l'instance d'un nouveau scoreboard
+     * Initializes keywords.
+     * @see {@link ScoreboardWrapper#tk}
+     * @see {@link ScoreboardWrapper#lk}
      */
-    private final Scoreboard scoreboard = Bukkit.getScoreboardManager().getNewScoreboard();
+    private void initializeKeywords()
+    {
+        try
+        {
+            tk = new TimerKeyword("timer");
+            lk = new LocationKeyword("location");
+        }catch(Exception e0)
+        {
+            e0.printStackTrace();
+        }
+    }
+
 
     /**
-     * Logger dédié de la classe.
+     * Loads scoreboard configuration file in the memory
+     * @see {@link ScoreboardWrapper#scoreboardConfig}
      */
-    private static final BasicLogger LOGGER = LoggerFactory.getLogger(ScoreboardWrapper.class);
-
-    private ConfigYamlFile scoreboardConfig;
-
+    private void loadScoreboardConfigFile()
     {
         try
         {
@@ -37,11 +127,6 @@ public class ScoreboardWrapper implements ProgramConstants
         {
             LOGGER.error(die0.getMessage());
         }
-    }
-
-    public ScoreboardWrapper()
-    {
-
     }
 
     /**
